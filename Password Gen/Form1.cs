b49162abae2b0e0e67a4f788b52e33ee503366f9
Matthew Steed <matthewsteedmusic@googlemail.com>
2022-02-12 07:58:50 +0000

    Add project files.

---
+++ Password Gen/Form1.cs
@@ -0,0 +1,35 @@
+namespace Password_Gen
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rndLC = new Random();
+            Random rndUC = new Random();
+            Random rndNN = new Random();
+            Random rndSP = new Random();
+            string pass = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                label1.Text = "";
+                char randomChar = (char)rndLC.Next('a', 'z');
+                pass += randomChar;
+                char randomChar2 = (char)rndUC.Next('A', 'Z');
+                pass += randomChar2;
+                char randomCharN = (char)rndNN.Next('0', '9');
+                pass += randomCharN;
+                char randomCharS = (char)rndSP.Next('!', '+');
+                pass += randomCharS;
+                label1.Text += pass;
+            }
+        }
+
+    }
+}
